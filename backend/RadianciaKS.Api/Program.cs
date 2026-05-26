e62821dac128872317ae9d322752721e6c5399d9
Dérick Pipoca <derickwfsilva@gmail.com>
2026-05-26 15:38:59 -0300

    feat: implemented tenant header filter, controllers and some dependency injections

--- backend/RadianciaKS.Api/Program.cs
+++ backend/RadianciaKS.Api/Program.cs
@@ -1,21 +1,35 @@
+using FluentValidation;
 using Microsoft.EntityFrameworkCore;
 using RadianciaKS.Api.Services;
 using RadianciaKS.Application.Interfaces;
+using RadianciaKS.Application.Services;
+using RadianciaKS.Application.Services.Interfaces;
 using RadianciaKS.Infrastructure.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.OperationFilter<RadianciaKS.Api.Swagger.TenantHeaderFilter>();
+});
 
 builder.Services.AddHttpContextAccessor();
 
 //Dependency Injections
 builder.Services.AddScoped<ITenantProvider, TenantProvider>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+
+builder.Services.AddValidatorsFromAssembly(typeof(CategoryService).Assembly);
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
+
+builder.Services.AddScoped<IApplicationDbContext>(provider => provider.GetRequiredService<ApplicationDbContext>());
+
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -24,6 +38,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.MapControllers();
 
 app.UseHttpsRedirection();
 
