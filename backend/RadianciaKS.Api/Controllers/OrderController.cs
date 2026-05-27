35d250753bbd89c3e2ab407633666b41cf2024f9
Dérick Pipoca <derickwfsilva@gmail.com>
2026-05-27 16:14:20 -0300

    feat: order controller

---
+++ backend/RadianciaKS.Api/Controllers/OrderController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using RadianciaKS.Application.DTOs.Order;
+using RadianciaKS.Application.Services.Interfaces;
+
+namespace RadianciaKS.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrderController : ControllerBase
+    {
+        private readonly IOrderService _orderService;
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateOrder([FromBody] OrderRequestDto dto)
+        {
+            var order = await _orderService.CreateOrder(dto);
+            return CreatedAtAction(nameof(CreateOrder), new { id = order.Id }, order);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllOrders()
+        {
+            var orders = await _orderService.GetAllOrders();
+
+            return Ok(orders);
+        }
+    }
+}
