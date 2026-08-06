bdde6d626a59ba1a5449ca4471934bd563a75748
Dérick Pipoca <derickwfsilva@gmail.com>
2026-08-06 10:15:05 -0300

    feat: implement prepared order scrolling bar display

--- frontend/src/app/features/pdv/layout/layout.ts
+++ frontend/src/app/features/pdv/layout/layout.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { OrderResponseDto } from './../../../core/models/order.model';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OrderService } from '../../../core/services/order-service';
+import { OrderStatus } from '../../../core/enums/order-status';
+import { SignalrService } from '../../../core/services/signalr-service';
+import { debounceTime, merge, Subscription } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './layout.html',
   styleUrl: './layout.scss',
 })
-export class Layout {}
+export class Layout implements OnInit, OnDestroy {
+  private orderService = inject(OrderService);
+  private signalrService = inject(SignalrService);
+  private subscriptions = new Subscription();
+
+  readyOrders: OrderResponseDto[] = [];
+
+  ngOnInit(): void {
+    this.loadReadyOrders();
+    this.signalrService.startConnection();
+
+    this.subscriptions.add(
+      merge(this.signalrService.newItem$, this.signalrService.itemReady$)
+        .pipe(debounceTime(300))
+        .subscribe(() => {
+          this.loadReadyOrders();
+        }),
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+
+  loadReadyOrders(): void {
+    let params = {
+      pageNumber: 1,
+      pageSize: 250,
+      sortBy: 'createdat',
+      status: OrderStatus.ReadyToServe,
+    };
+    this.orderService.getAll(params).subscribe({
+      next: (response) => {
+        this.readyOrders = response.data;
+        console.log(response.data);
+      },
+      error: (err) => console.error('Erro ao buscar pedidos na API:', err),
+    });
+  }
+}
