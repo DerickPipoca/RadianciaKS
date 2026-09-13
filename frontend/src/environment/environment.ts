7279903cb5b7759435d249dac56b045f02f9604a
Dérick Pipoca <derickwfsilva@gmail.com>
2026-09-13 11:59:08 -0300

    feat: add Docker with Dockerfile and docker-compose for backend and frontend

--- frontend/src/environment/environment.ts
+++ frontend/src/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: `http://${window.location.hostname}:5228/api`,
-  serverUrl: `http://${window.location.hostname}:5228`,
+  apiUrl: `http://${window.location.hostname}:8080/api`,
+  serverUrl: `http://${window.location.hostname}:8080`,
 };
